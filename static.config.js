e0e02223f726d2e871f443fab0580fb7aab5dbdc
taze-fullstack <taze@fullstack.ph>
2018-09-10 19:18:50 +0800

    amp sample

--- static.config.js
+++ static.config.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 const fs = require('fs')
 const klaw = require('klaw')
 const path = require('path')
@@ -41,11 +43,19 @@ function getPosts () {
   return getFiles()
 }
 
-export default {
+export class InitialHead extends React.Component {
+  render() {
+    return (
+      <div>What</div>
+    )
+  }
+}
 
+export default {
   getSiteData: () => ({
     title: 'React Static with Netlify CMS',
   }),
+  siteRoot: 'https://react-amp-netlify.netlify.com/',
   getRoutes: async () => {
     const posts = await getPosts()
     return [
@@ -76,5 +86,35 @@ export default {
         component: 'src/containers/404',
       },
     ]
+  },
+  Document: class CustomHTML extends React.Component {
+    render() {
+      const { Html, Head, Body, children, renderMeta } = this.props;
+      const defaultStyle = `
+        body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}
+      `
+      const noScriptStyle = `
+        body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}
+      `
+      return (
+        <Html lang="en-US" amp="true">
+          <Head>
+            <meta charSet="UTF-8" />
+            <script async src="https://cdn.ampproject.org/v0.js"></script>
+            <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
+            <meta name="fragment" content="!" />
+            <link rel="canonical" href="https://react-amp-netlify.netlify.com/"></link>
+            <style amp-boilerplate="true" dangerouslySetInnerHTML={{ __html: defaultStyle }}></style>
+            <noscript>
+              <style amp-boilerplate="true" dangerouslySetInnerHTML={{ __html: noScriptStyle }}></style>
+            </noscript>
+            <title>Hello AMP</title>
+          </Head>
+          <Body>
+            {children}
+          </Body>
+        </Html>
+      )
+    }
   },
 }
